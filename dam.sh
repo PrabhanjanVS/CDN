8cce3a4ecaf7d4eb16ed33e40023e95e21307d1b
Prabhanjan-V-Sangam <prabhanjanv.sangam@gmail.com>
2025-08-09 01:00:59 +0530

    helm blueprint ready

---
+++ dam.sh
@@ -0,0 +1,22 @@
+
+wget https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_640x360.m4v -O BigBuckBunny.mp4
+
+kubectl apply -f - <<EOF
+apiVersion: v1
+kind: Pod
+metadata:
+  name: video-uploader
+spec:
+  containers:
+  - name: uploader
+    image: nginx:alpine  # Replace with your actual app image
+    volumeMounts:
+    - name: nginx-storage
+      mountPath: /videos
+  volumes:
+  - name: nginx-storage
+    persistentVolumeClaim:
+      claimName: nginx-storage
+EOF
+
+kubectl cp BigBuckBunny.mp4 video-uploader:/videos/example.mp4
